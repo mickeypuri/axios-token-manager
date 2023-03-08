8dbb6b85f1e5d4115ad15205a141c8b8a3a7f2dd
Mickey Puri <mickeypuri@hotmail.com>
2023-03-08 16:56:57 +0000

    rule out option of no response in the error object

--- src/utils/shouldRefresh.ts
+++ src/utils/shouldRefresh.ts
@@ -2,6 +2,9 @@ import { AxiosError } from 'axios';
 import { IConfig } from '../types';
 
 export const shouldRefresh = (error: AxiosError, options: IConfig, refreshTries: number) => {
+    if (!error.response) {
+        return false;
+    }
     const { response: { status } = {} } = error;
     const { refreshOnStatus, maxRefreshTries } = options;
     const authFailed = refreshOnStatus.includes(status as number);
