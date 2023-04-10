3b9e9f96181253f86587fc41e0f003bdceefdcdf
Mickey Puri <mickeypuri@hotmail.com>
2023-04-10 18:45:37 +0100

    added more caching tests

--- src/tests/caching.test.ts
+++ src/tests/caching.test.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 import nock from 'nock';
 import { IToken, TokenProvider } from '../types';
-import { defaultSettings } from '../utils/initialValues';
 import tokenManager from '../tokenManager';
 import { getState } from '../state';
 
@@ -14,23 +13,6 @@ const token_one: IToken = { access_token: 'token 1',
     expires_in: 300,
     scope: 'scope'
 };
-
-const token_two: IToken = { access_token: 'token 2',
-    token_type: 'Bearer',
-    expires_in: 300,
-    scope: 'scope'
-};
-
-const getMockAxiosInstance = () => ({
-    interceptors: {
-        request: {
-            use: jest.fn()
-        },
-        response: {
-            use: jest.fn()
-        }
-    }
-}) as unknown as AxiosInstance;
 
 beforeAll(() => {
     nock(baseURL, {
@@ -60,16 +42,26 @@ describe('tokenManager caching', () => {
     it('calls for a Token when it gets a request and no token exists', async () => {
         const getCredentials: TokenProvider = jest.fn();
         const instance = axios.create({ baseURL });
-        (getCredentials as jest.Mock).mockResolvedValueOnce(token_one);
+        (getCredentials as jest.Mock).mockResolvedValue(token_one);
         tokenManager({ instance, getCredentials });
         await instance.get(`${baseURL}${channelsPath}`);
+        expect((getCredentials as jest.Mock)).toBeCalledTimes(1);
+    });
+
+    it('caches the Token after getting it', async () => {
+        const getCredentials: TokenProvider = jest.fn();
+        const instance = axios.create({ baseURL });
+        (getCredentials as jest.Mock).mockResolvedValue(token_one);
+        tokenManager({ instance, getCredentials });
+        await instance.get(`${baseURL}${channelsPath}`);
+
         expect((getCredentials as jest.Mock)).toBeCalledTimes(1);
     });
 
     it('after start up it gets token in first call, then uses cached token for next four calls', async () => {
         const getCredentials: TokenProvider = jest.fn();
         const instance = axios.create({ baseURL });
-        (getCredentials as jest.Mock).mockResolvedValueOnce(token_one);
+        (getCredentials as jest.Mock).mockResolvedValue(token_one);
         tokenManager({ instance, getCredentials });
 
         await instance.get(`${baseURL}${channelsPath}`);
@@ -81,5 +73,20 @@ describe('tokenManager caching', () => {
         expect((getCredentials as jest.Mock)).toBeCalledTimes(1);
     });
 
+    it('after getting a token, it uses cached token for next four simultaneous calls', async () => {
+        const getCredentials: TokenProvider = jest.fn();
+        const instance = axios.create({ baseURL });
+        (getCredentials as jest.Mock).mockResolvedValue(token_one);
+        tokenManager({ instance, getCredentials });
 
+        await instance.get(`${baseURL}${channelsPath}`);
+        await Promise.all([
+            instance.get(`${baseURL}${channelsPath}`),
+            instance.get(`${baseURL}${channelsPath}`),
+            instance.get(`${baseURL}${channelsPath}`),
+            instance.get(`${baseURL}${channelsPath}`)
+        ]);
+
+        expect((getCredentials as jest.Mock)).toBeCalledTimes(1);
+    });
 });
