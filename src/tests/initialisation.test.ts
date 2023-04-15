9ebbd12862af5bc5b268bebde22e348b8964c94d
Mickey Puri <mickeypuri@hotmail.com>
2023-04-15 19:10:40 +0100

    formatted document

--- src/tests/initialisation.test.ts
+++ src/tests/initialisation.test.ts
@@ -18,55 +18,55 @@ const getMockAxiosInstance = () => ({
 }) as unknown as AxiosInstance;
 
 describe('tokenManager initialisation', () => {
-        it('sets options to default settings if values not provided', () => {
-            const instance = getMockAxiosInstance();
-            tokenManager({ instance, getCredentials });
-            const { options } = getState();
-            expect(options).toEqual(defaultSettings);
-        });
+    it('sets options to default settings if values not provided', () => {
+        const instance = getMockAxiosInstance();
+        tokenManager({ instance, getCredentials });
+        const { options } = getState();
+        expect(options).toEqual(defaultSettings);
+    });
 
-        it('sets rest of options to default settings and uses two options provided', () => {
-            const instance = getMockAxiosInstance();
+    it('sets rest of options to default settings and uses two options provided', () => {
+        const instance = getMockAxiosInstance();
 
-            const maxRecoveryTries = 10;
-            const onAuthFail = () => console.log('Authorisation has failed');
-            const expectedOptions = {...defaultSettings, maxRecoveryTries, onAuthFail };
+        const maxRecoveryTries = 10;
+        const onAuthFail = () => console.log('Authorisation has failed');
+        const expectedOptions = {...defaultSettings, maxRecoveryTries, onAuthFail };
 
-            tokenManager({ instance, getCredentials, maxRecoveryTries, onAuthFail });
-            const { options } = getState();
-            expect(options).toEqual(expectedOptions);
-        });
+        tokenManager({ instance, getCredentials, maxRecoveryTries, onAuthFail });
+        const { options } = getState();
+        expect(options).toEqual(expectedOptions);
+    });
 
-        it('sets rest of options to default settings and uses the three options provided', () => {
-            const instance = getMockAxiosInstance();
+    it('sets rest of options to default settings and uses the three options provided', () => {
+        const instance = getMockAxiosInstance();
 
-            const tokenTryThreshold = 10;
-            const onTokenRefresh = () => console.log('Authorisation has failed');
-            const header = 'Test Header';
-            const expectedOptions = {...defaultSettings, tokenTryThreshold, onTokenRefresh, header };
+        const tokenTryThreshold = 10;
+        const onTokenRefresh = () => console.log('Authorisation has failed');
+        const header = 'Test Header';
+        const expectedOptions = {...defaultSettings, tokenTryThreshold, onTokenRefresh, header };
 
-            tokenManager({ instance, getCredentials, tokenTryThreshold, onTokenRefresh, header });
-            const { options } = getState();
-            expect(options).toEqual(expectedOptions);
-        });
+        tokenManager({ instance, getCredentials, tokenTryThreshold, onTokenRefresh, header });
+        const { options } = getState();
+        expect(options).toEqual(expectedOptions);
+    });
 
-        it('is set up to intercept all requests', () => {
-            const instance = getMockAxiosInstance();
-            tokenManager({ instance, getCredentials });
-            const { interceptors : { request : { use : requestInterceptMock }}} = instance;
-            expect((requestInterceptMock as jest.Mock)).toBeCalledTimes(1);
-        });
+    it('is set up to intercept all requests', () => {
+        const instance = getMockAxiosInstance();
+        tokenManager({ instance, getCredentials });
+        const { interceptors : { request : { use : requestInterceptMock }}} = instance;
+        expect((requestInterceptMock as jest.Mock)).toBeCalledTimes(1);
+    });
 
-        it('is set up to intercept all responses', () => {
-            const instance = getMockAxiosInstance();
-            tokenManager({ instance, getCredentials });
-            const { interceptors : { response : { use : responseInterceptMock }}} = instance;
-            expect((responseInterceptMock as jest.Mock)).toBeCalledTimes(1);
-        });
+    it('is set up to intercept all responses', () => {
+        const instance = getMockAxiosInstance();
+        tokenManager({ instance, getCredentials });
+        const { interceptors : { response : { use : responseInterceptMock }}} = instance;
+        expect((responseInterceptMock as jest.Mock)).toBeCalledTimes(1);
+    });
 
-        it('will not invoke getCredentials at initialisation', () => {
-            const instance = getMockAxiosInstance();
-            tokenManager({ instance, getCredentials });
-            expect((getCredentials as jest.Mock)).not.toBeCalled();
-        });
+    it('will not invoke getCredentials at initialisation', () => {
+        const instance = getMockAxiosInstance();
+        tokenManager({ instance, getCredentials });
+        expect((getCredentials as jest.Mock)).not.toBeCalled();
+    });
 });
