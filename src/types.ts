253d7718bd80d18aaab3f808e3c6f7f5fbb48dc7
Mickey Puri <mickeypuri@hotmail.com>
2023-04-23 19:49:02 +0100

    reformatted to semi colons

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
 import { AxiosInstance } from 'axios';
 
 export interface ICache {
-    token: IToken | null,
-    expiration: number
+    token: IToken | null;
+    expiration: number;
 }
 
 export interface ITokenManager extends DefaultSettings {
-    instance: AxiosInstance,
-    getCredentials: TokenProvider,
+    instance: AxiosInstance;
+    getCredentials: TokenProvider;
 }
 
 export type LogFunction = (message?: string) => void;
@@ -15,35 +15,35 @@ export type LogFunction = (message?: string) => void;
 type DefaultSettings = Partial<IConfig>;
 
 export interface IConfig {
-    refreshBuffer: number,
-    header: string,
-    formatter: Formatter,
-    onTokenRefresh: LogFunction,
-    onAuthFail: LogFunction,
-    onTokenRequestFail: VoidFunction,
-    refreshOnStatus: number [],
-    tokenTryThreshold: number,
-    onTokenTryThreshold: (retries: number) => void,
-    onRecoveryTry: LogFunction,
-    maxRecoveryTries: number,
-    onRecoveryAbort: VoidFunction,
-    addTokenToLogs: boolean,
+    refreshBuffer: number;
+    header: string;
+    formatter: Formatter;
+    onTokenRefresh: LogFunction;
+    onAuthFail: LogFunction;
+    onTokenRequestFail: VoidFunction;
+    refreshOnStatus: number [];
+    tokenTryThreshold: number;
+    onTokenTryThreshold: (retries: number) => void;
+    onRecoveryTry: LogFunction;
+    maxRecoveryTries: number;
+    onRecoveryAbort: VoidFunction;
+    addTokenToLogs: boolean;
 }
 
 export interface IToken {
-    access_token: string
-    token_type: string,
-    expires_in: number,
-    scope: string
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    scope: string;
 }
 
 export interface IState {
-    cache: ICache,
-    options: IConfig,
-    tokenTries: number,
-    recoveryTries: number,
-    inRecovery: boolean,
-    getCredentials: TokenProvider,
+    cache: ICache;
+    options: IConfig;
+    tokenTries: number;
+    recoveryTries: number;
+    inRecovery: boolean;
+    getCredentials: TokenProvider;
 }
 
 export type TokenProvider = () => Promise<IToken>;
