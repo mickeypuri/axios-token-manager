0dbf214505e3feea8da0906e3d082324353c4514
Mickey Puri <mickeypuri@hotmail.com>
2023-04-25 23:47:17 +0100

    test getToken returns a rejected promise when there is an error getting a fresh token

--- src/tests/getToken.test.ts
+++ src/tests/getToken.test.ts
@@ -2,6 +2,7 @@ import { getToken } from '../utils/getToken';
 import { getState } from '../state';
 import { TokenProvider } from '../types';
 import Semaphore from 'semaphore-async-await';
+import { getFreshToken } from '../utils/getFreshToken';
 
 const ACCESS_TOKEN_ONE = 'Token One';
 const ACCESS_TOKEN_TWO = 'Token Two';
@@ -32,8 +33,13 @@ const validCache = {
 };
 
 jest.mock('../state');
+jest.mock('../utils/getFreshToken');
 
-describe.only ('getToken', () => {
+afterEach(() => {
+    jest.clearAllMocks();
+})
+
+describe ('getToken', () => {
     it('will not get a fresh token if the previous request has already updated the token', async () => {
         const getCredentials: TokenProvider = jest.fn();
         const lock = {
@@ -57,5 +63,31 @@ describe.only ('getToken', () => {
         expect((lock.release as jest.Mock)).toBeCalledTimes(1);
         expect((getCredentials as jest.Mock)).not.toBeCalled();
         expect(token).toEqual(validToken);
+    });
+
+    it('will return a rejected promise if there is an error getting the fresh token', async () => {
+        const getCredentials: TokenProvider = jest.fn();
+        const lock = {
+            acquire: jest.fn().mockResolvedValueOnce(''),
+            release: jest.fn()
+        } as unknown as Semaphore;
+
+        (getState as jest.Mock).mockReturnValue({
+            cache: expiredCache,
+            getCredentials
+        });
+
+        const getFreshTokenError = new Error('Error getting fresh Token');
+        (getFreshToken as jest.Mock).mockRejectedValueOnce(getFreshTokenError);
+
+        let actualError = null;
+        try {
+            await getToken(lock);
+        } catch (error) {
+            actualError = error;
+        }
+
+        expect(actualError).toEqual(getFreshTokenError);
+        expect((lock.release as jest.Mock)).toBeCalledTimes(1);        
     });
 });
