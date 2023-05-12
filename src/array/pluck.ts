666aa4f1b4bc6ea9ebdbb7fd41600fc0df040318
mickeypuri <mickeypuri@hotmail.com>
2023-05-12 19:55:32 +0100

    removed redundant code

--- src/array/pluck.ts
+++
@@ -1,6 +0,0 @@
-export const pluck : PluckType = (elements , field) => {
-    return elements.map(element => element[field]);
-};
-
-export type PluckType = (elements: Record<string, string>[], field: string) => string [];
-
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import tokenManager from './tokenManager';
 export default tokenManager;
-export { pluck, PluckType } from './array/pluck';
 export { Settings, TokenProvider, IToken, Formatter } from './types';
